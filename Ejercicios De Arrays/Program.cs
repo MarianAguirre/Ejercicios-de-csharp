3567daa653192db939ca0b22094997ff35da92b1
Marian <marian.aguirre.carrill@gmail.com>
2025-09-13 13:18:36 -0600

    Inicio de los Ejercicios con Arrays

---
+++ Ejercicios De Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿class Arrays
+{
+  void Array1()
+  {
+    string[] primerArray = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+    Console.WriteLine(string.Join(" ",primerArray));
+  }
+  static void Main(string[] args)
+  {
+    string opcion;
+    Arrays resolucion = new Arrays();
+
+    Console.WriteLine("EJERCICIOS CON ARRAYS");
+    do
+    {
+      Console.WriteLine("=========================");
+      Console.WriteLine("Elige un numero para ver el ejercicio...");
+      for (int ea = 1; ea <= 7; ea++)
+      {
+        Console.WriteLine(ea);
+      }
+      opcion = Console.ReadLine() ?? "";
+
+      switch (opcion)
+      {
+        case "1":
+          Console.WriteLine("1) Crea un array de 10 posiciones, con valores puestos por ti y muestra el array.");
+          resolucion.Array1();
+          break;
+        default:
+          Console.WriteLine("OPCION INVALIDA");
+          break;
+      }
+
+    } while (opcion != "7");
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Ejercicios-de-C#
 Ejercicios prácticos de C#
 
+### Ejercicio - Cajero Automatico
+Ejercicio practico basico de la funcion de un cajero automatico, ejercicio creado del video [EJERCICIO - CAJERO AUTOMATICO](https://www.youtube.com/watch?v=DffpxkMj2jY&list=PL2Z95CSZ1N4F7mym8Ca16RoTDVAlIOPlT&index=30)
+
+### Ejercicios propuestos básicos de C#
+10 Ejercicios básicos para practicar C#, ejercicios originarios de [Ejercicios propuestos y resueltos básicos en C#](https://discoduroderoer.es/ejercicios-propuestos-y-resueltos-basicos-en-c-sharp/)
+
+### Ejercicios de Arrays [EN PROGRESO]
+7 Ejercicios de arrays, originarios de [Ejercicios de arrays propuestos y resueltos de C#](https://discoduroderoer.es/ejercicios-de-arrays-propuestos-y-resueltos-de-c-sharp/)
